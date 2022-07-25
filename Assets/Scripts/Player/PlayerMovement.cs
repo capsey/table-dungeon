e7488aac14b3a46564df491952f0b5c782a7d549
Caps Lock <46106832+capsey@users.noreply.github.com>
2022-07-25 20:15:41 +0600

    Dungeon view Player sprite added

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,13 +7,22 @@ namespace TableDungeon.Player
     {
         public float speed = 5.0F;
         public float acceleration = 1.0F;
-    
+
+        private SpriteRenderer _renderer;
+        private Animator _animator;
+        
         private Rigidbody2D _rigidbody;
         private Vector2 _targetVelocity;
         private GameManager _manager;
+        
+        private static readonly int MoveEnded = Animator.StringToHash("Move Ended");
+        private static readonly int Player = Animator.StringToHash("Player");
+        private static readonly int Moving = Animator.StringToHash("Moving");
 
         private void Awake()
         {
+            _renderer = GetComponent<SpriteRenderer>();
+            _animator = GetComponent<Animator>();
             _rigidbody = GetComponent<Rigidbody2D>();
         }
 
@@ -22,17 +31,22 @@ namespace TableDungeon.Player
             _manager = FindObjectOfType<GameManager>();
             _manager.Controls.Dungeon.Movement.performed += OnInputMovement;
             _manager.Controls.Dungeon.Movement.canceled += OnInputMovement;
-            _manager.OnStateChanged += (s, p, changed) =>
+            _manager.OnStateChanged += (s, player, changed) =>
             {
                 if (!changed) return;
                 transform.localPosition = Vector3.zero;
                 _rigidbody.velocity = Vector2.zero;
+                _animator.SetBool(Player, player);
+                _animator.SetTrigger(MoveEnded);
             };
         }
 
         private void OnInputMovement(InputAction.CallbackContext ctx)
         {
             _targetVelocity = ctx.ReadValue<Vector2>() * speed;
+            if (_targetVelocity.x > 0) _renderer.flipX = false;
+            else if (_targetVelocity.x < 0) _renderer.flipX = true;
+            _animator.SetBool(Moving, _targetVelocity.sqrMagnitude > 0);
         }
     
         private void Update()
